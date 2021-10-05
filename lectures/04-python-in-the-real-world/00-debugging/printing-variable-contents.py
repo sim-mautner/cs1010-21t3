2db1ce9b80913c14736f7f8998cea72184989061
Sim Mautner <s.mautner@unsw.edu.au>
2021-10-05 14:20:55 +1100

    lectures 4 and 5.1

---
+++ lectures/04-python-in-the-real-world/00-debugging/printing-variable-contents.py
@@ -0,0 +1,16 @@
+# import libraries (if you have them)
+
+# define constants (if you have them)
+
+# other functions you write (if you have them)
+
+def main():
+    name = "Sim"
+    tutor_name = "Will"
+    # print(f"name = {name}")
+    print(f"{name=}")
+    # print(f"tutor_name = {tutor_name}")
+    print(f"{tutor_name=}")
+
+if __name__ == '__main__':
+    main()
---
+++ lectures/04-python-in-the-real-world/01-examples/01-tic-tac-toe.py
@@ -0,0 +1,41 @@
+# Put your imports here
+
+# Put your constants here
+PLAYER_0 = 'X'
+PLAYER_1 = 'O'
+BOARD_SIZE = 5
+
+# Functions
+def print_board(board):
+    for row in board:
+        #print(row)
+        for cell in row:
+            print(cell + ' ', end='')
+        print('\n')
+
+def create_board():
+   board = []
+   for row in range(BOARD_SIZE):
+      board.append(['-']*BOARD_SIZE)
+   return board
+
+# Main function
+def main():
+
+   board = create_board()
+
+   player = PLAYER_0
+   while True:
+      print_board(board)
+      row = int(input("Enter row: "))
+      col = int(input("Enter col: "))
+      board[row][col] = player
+      if player == PLAYER_0:
+         player = PLAYER_1
+      else:
+         player = PLAYER_0
+
+
+# Code which gets executed at the start
+if __name__ == '__main__':
+   main()
